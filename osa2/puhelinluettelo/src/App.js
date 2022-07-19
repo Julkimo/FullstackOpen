a38470ab5c8346ddce22c9176cb60b494e593a4e
Julkimo <santeri.julkunen@gmail.com>
2022-07-19 13:34:11 +0300

    osa2c commit
    -2.11 tehty, 2.12-2.14 eivät

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ( {filterText, handleFiltering} ) => {
   return (
@@ -47,12 +48,17 @@ const Persons = ( {personsToShow} ) => {
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 0 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 }
-  ])
+  const [persons, setPersons] = useState([ ])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const [nextId, setNextId] = useState(persons.length)
   const [newNumber, setNewNumber] = useState('')
